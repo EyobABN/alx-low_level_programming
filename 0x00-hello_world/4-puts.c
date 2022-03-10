a056814d9756cb3baf545bbfc422c33e744d1399
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-10 18:15:26 +0300

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * This program prints "\"Programming is like building a
+ * multilingual puzzle." using the function puts
+*/
+
+int main(void)
+{
+/* print a string using the puts function */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
