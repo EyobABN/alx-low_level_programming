eb2ba3bb597a2293c1980d281337130020318982
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-10 18:34:09 +0300

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * This program prints "with proper grammar, but the
+ * outcome is a piece of art,." using the function puts
+*/
+
+/**
+ * main - print a string using the puts function
+ * Return: return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
