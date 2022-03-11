63f08bf51751792d7625b5ff2a8d60b1b29b226c
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-11 14:04:00 +0300

    Add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
