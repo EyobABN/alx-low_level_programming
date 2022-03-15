63dde551c486291a60328be5008d0908ada44287
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-15 22:57:09 +0300

    Add 8-24_hours.c and update main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every second
+ *
+ * Return: value of last digit
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 2 && j == 4)
+				break;
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
