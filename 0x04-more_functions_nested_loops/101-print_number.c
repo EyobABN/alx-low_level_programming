0bf51e51e37fe6434434ae1876517a73ce274ee5
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-18 00:58:51 +0300

    Add 101-print_number.c and update main.h

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer recursively using _putchar
+ * @n: the integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	} else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+
+		if (n == 0)
+			_putchar('0');
+
+		if (n / 10)
+			print_number(n / 10);
+
+		_putchar(n % 10 + '0');
+	}
+}
