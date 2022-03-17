eccb863974487573c8d89fbe678565593f919ad3
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-17 19:52:38 +0300

    Add 1-isdigit.c and update main.h

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
