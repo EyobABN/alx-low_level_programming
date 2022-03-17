307415f5b9cd45908c4bded7b6ec599fbb46e95d
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-17 22:05:07 +0300

    Add 10-print_triangle.c and update main.h

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints square of hashes,
+ * @size: the dimension of the square
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j < (i + 1); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
