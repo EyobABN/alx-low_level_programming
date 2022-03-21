0ac046fe79476db356999b25e2ef536adefbf3d4
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-21 14:55:00 +0300

    Add 4-print_rev.c and update main.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: first character of the string to be reverse printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	while (count >= 0)
+	{
+		_putchar(*(s + count));
+		count--;
+	}
+	_putchar('\n');
+}
