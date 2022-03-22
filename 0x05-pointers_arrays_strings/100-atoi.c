27e3c64a781efa236a676dfa896b7024fa1e23a5
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-22 22:53:26 +0300

    Add 100-atoi.c and update main.h

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,6 +2,22 @@
 #include <limits.h>
 
 /**
+ * count_s - counts a string
+ * @c: the string's first char
+ *
+ * Return: the count
+ */
+int count_s(char *c)
+{
+	int count;
+
+	count = 0;
+	while (*(c + count))
+		count++;
+	return (count);
+}
+
+/**
  * _atoi - convert string into number
  * @s: string to be converted
  *
@@ -11,11 +27,7 @@ int _atoi(char *s)
 {
 	int count, i, sign, sum, pisn, j, digit, first, p;
 
-	count = 0;
-	while (*(s + count))
-	{
-		count++;
-	}
+	count = count_s(s);
 	sign = 1;
 	sum = 0;
 	pisn = 0;
@@ -29,8 +41,7 @@ int _atoi(char *s)
 				break;
 			if (*(s + i) == 45)
 				sign = -sign;
-		}
-		else
+		} else
 		{
 			if (first)
 			{
@@ -48,8 +59,7 @@ int _atoi(char *s)
 		{
 			if (sign == 1)
 				return (INT_MAX);
-			else
-				return (INT_MIN);
+			return (INT_MIN);
 		}
 		sum += p * (*(s + i) - 48);
 		p = p * 10;
