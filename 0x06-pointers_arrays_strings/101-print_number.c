da410010601a2cf82dc285aa008aed3c0f3677ea
EyobABN <eyob.alamerew.belay@gmail.com>
2022-03-24 13:43:04 +0300

    Add 101-print_number.c and update main.h

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,20 +8,27 @@
  */
 void print_number(int n)
 {
-	if (n == 0)
+	int i, cpy, digit, power;
+
+	if (n < 0)
 	{
-		_putchar('0');
-	} else
+		_putchar('-');
+		n = -n;
+	}
+	cpy = n;
+	digit = 1;
+	while (cpy / 10 > 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			n = -n;
-		}
-		if (n == 0)
-			_putchar('0');
-		if (n / 10)
-			print_number(n / 10);
-		_putchar(n % 10 + '0');
+		cpy = cpy / 10;
+		digit++;
+	}
+	power = 1;
+	for (i = 0; i < digit - 1; i++)
+		power = power * 10;
+	for (i = 0; i < digit; i++)
+	{
+		_putchar((n / power) + '0');
+		n = n % power;
+		power = power / 10;
 	}
 }
