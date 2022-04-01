b11a27e9b8a8f10238445aee2e89e232de5e5ec3
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-01 16:04:32 +0300

    Add 0-whatsmyname.c and update main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of this program
+ * @argc: number of command line arguments
+ * @argv: array of argument strings
+ *
+ * Return: always 0
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
