3d7f1a050da01bb0110973f79296d871f96e2c2c
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-04 20:13:55 +0300

    Add 3-alloc_grid.c and update main.h

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,19 @@ int **alloc_grid(int width, int height)
 
 	arr = (int **)malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			free(arr);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
