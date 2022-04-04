91b638775ec1cd810b113729d5d15691e03f42fc
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-04 20:10:10 +0300

    Add 4-free_grid.c and update main.h

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid
+ * @grid: a double pointer to the 2D grid
+ * @height: the number of rows of the grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
