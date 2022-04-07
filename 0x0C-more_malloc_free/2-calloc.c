69ec3589fccab981509e231651ed483dff206292
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-07 16:58:25 +0300

    Add 2-calloc.c and update main.h

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,15 +10,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *arr;
+/*	unsigned int i;*/
+	void *arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		arr[i] = 0;
-	return ((void *)arr);
+/*	for (i = 0; i < nmemb; i++)
+		arr++ = 0;*/
+	return (arr);
 }
