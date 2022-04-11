d64dc684831817c1b9a8779a74a3f7fbdc0dccc8
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-11 20:10:31 +0300

    Add 4-new_dog.c and update dog.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,43 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * print_dog - prints a struct dog
- * @d: pointer to the struct dog
+ * new_dog - creates a new dog
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
  *
- * Return: void
+ * Return: a pointer to the new dog
  */
-void print_dog(struct dog *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, size_name, size_owner;
 	char *n, *o;
+	dog_t *n_dog;
+	dog_t n_dg;
 
-	if (d == NULL)
-		return;
-	n = (d->name == NULL) ? "(nil)" : d->name;
-	o = (d->owner == NULL) ? "(nil)" : d->owner;
-	printf("Name: %s\nAge: %f\nOwner: %s\n", n, d->age, o);
+	n_dog = &n_dg;
+	size_name = 0;
+	for (i = 0; name[i] != '\0'; i++)
+		size_name++;
+	size_owner = 0;
+	for (i = 0; owner[i] != '\0'; i++)
+		size_owner++;
+	n = malloc(sizeof(char) * (size_name + 1));
+	if (n == NULL)
+		return (NULL);
+	o = malloc(sizeof(char) * (size_owner + 1));
+	if (o == NULL)
+		return (NULL);
+
+	for (i = 0; name[i] != '\0'; i++)
+		n[i] = name[i];
+	n[i] = name[i];
+	for (i = 0; owner[i] != '\0'; i++)
+		o[i] = owner[i];
+	o[i] = owner[i];
+	n_dog->name = n;
+	n_dog->age = age;
+	n_dog->owner = o;
+	return (n_dog);
 }
