307762f2a92af1d09eba51a9b7478ef3bede1cd1
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-12 00:06:56 +0300

    Add 4-new_dog.c and update dog.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, name_size, owner_size;
-	char *name_cpy, *owner_cpy;
 	dog_t *n_dog;
 
 	n_dog = (dog_t *)malloc(sizeof(*n_dog));
@@ -21,37 +20,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	name_size = 0;
 	for (i = 0; name[i] != '\0'; i++)
 		name_size++;
+
 	owner_size = 0;
 	for (i = 0; owner[i] != '\0'; i++)
 		owner_size++;
-	name_cpy = calloc(name_size + 1, sizeof(char));
-	if (name_cpy == NULL)
-		return (NULL);
-	owner_cpy = calloc(owner_size + 1, sizeof(char));
-	if (owner_cpy == NULL)
-		return (NULL);
-	for (i = 0; name[i] != '\0'; i++)
-		name_cpy[i] = name[i];
-	name_cpy[i] = name[i];
-	for (i = 0; owner[i] != '\0'; i++)
-		owner_cpy[i] = owner[i];
-	owner_cpy[i] = owner[i];
 
 	n_dog->name = calloc(name_size + 1, sizeof(char));
 	if (n_dog->name == NULL)
 		return (NULL);
+
 	n_dog->age = age;
+
 	n_dog->owner = calloc(owner_size + 1, sizeof(char));
 	if (n_dog->owner == NULL)
 		return (NULL);
 
 	for (i = 0; name[i] != '\0'; i++)
-		n_dog->name[i] = name_cpy[i];
-	n_dog->name[i] = name_cpy[i];
-	free(name_cpy);
+		n_dog->name[i] = name[i];
+	n_dog->name[i] = name[i];
 	for (i = 0; owner[i] != '\0'; i++)
-		n_dog->owner[i] = owner_cpy[i];
-	n_dog->owner[i] = owner_cpy[i];
-	free(owner_cpy);
+		n_dog->owner[i] = owner[i];
+	n_dog->owner[i] = owner[i];
+
 	return (n_dog);
 }
