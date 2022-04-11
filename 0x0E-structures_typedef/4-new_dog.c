6de754e9e226f15c9e0b496c262bfa66eea172cc
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-11 21:20:44 +0300

    Add 4-new_dog.c and update dog.h

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,32 +12,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, size_name, size_owner;
-	char *n, *o;
 	dog_t *n_dog;
 	dog_t n_dg;
 
 	n_dog = &n_dg;
+
 	size_name = 0;
 	for (i = 0; name[i] != '\0'; i++)
 		size_name++;
+
 	size_owner = 0;
 	for (i = 0; owner[i] != '\0'; i++)
 		size_owner++;
-	n = malloc(sizeof(char) * (size_name + 1));
-	if (n == NULL)
+
+	n_dog->name = calloc(size_name + 1, sizeof(char));
+	if (n_dog->name == NULL)
 		return (NULL);
-	o = malloc(sizeof(char) * (size_owner + 1));
-	if (o == NULL)
+
+	n_dog->owner = calloc(size_owner + 1, sizeof(char));
+	if (n_dog->owner == NULL)
 		return (NULL);
 
 	for (i = 0; name[i] != '\0'; i++)
-		n[i] = name[i];
-	n[i] = name[i];
+		n_dog->name[i] = name[i];
+	n_dog->name[i] = name[i];
 	for (i = 0; owner[i] != '\0'; i++)
-		o[i] = owner[i];
-	o[i] = owner[i];
-	n_dog->name = n;
+		n_dog->owner[i] = owner[i];
+	n_dog->owner[i] = owner[i];
+
 	n_dog->age = age;
-	n_dog->owner = o;
 	return (n_dog);
 }
