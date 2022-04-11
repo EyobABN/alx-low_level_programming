a8d061051de8f092c2109353e27274da63c1c54c
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-11 18:01:20 +0300

    Add 2-print_dog.c and update dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to the struct dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
