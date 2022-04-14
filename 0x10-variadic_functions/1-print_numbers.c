8dc6e94cc45a03fae0e377ae3e3ab4e6636ebb9e
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-14 19:54:34 +0300

    Add 1-print_numbers.c and update variadic_functions.h

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers with the separator
+ * @separator: the separator
+ * @n: the number of integers
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != n - 1)
+			printf("%s", separator);
+		if (i == n - 1)
+			putchar('\n');
+	}
+	va_end(ap);
+}
