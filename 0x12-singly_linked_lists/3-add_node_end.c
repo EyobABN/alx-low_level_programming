858ef6b8b06c0f708933743c3b9f359d05309b6b
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-21 21:21:36 +0300

    Add 3-add_node_end.c and update lists.h

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - adds a node to the end of a list_t list
+ * @head: the head of the list_t list
+ * @str: the str element of the node to be added
+ *
+ * Return: the address of the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *p;
+
+	p = *head;
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		new->str = strdup("(nil)");
+		new->len = 0;
+	}
+	else
+	{
+		new->str = strdup(str);
+		new->len = strlen(new->str);
+	}
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (p->next != NULL)
+			p = p->next;
+		p->next = new;
+	}
+	return (new);
+}
