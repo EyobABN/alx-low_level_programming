d44f68d32834de8500b9fa4917903d45f7f87d61
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-21 22:02:11 +0300

    Add 4-free_list.c and update lists.h

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: the head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *p;
+
+	p = head;
+	if (p->next == NULL)
+	{
+		free(p->str);
+		free(p);
+		p = NULL;
+	}
+	else
+	{
+		free_list(p->next);
+		free(p->str);
+		free(p);
+		p = NULL;
+	}
+}
