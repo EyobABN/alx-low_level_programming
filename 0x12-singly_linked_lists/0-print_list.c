4d30cc9efd0813a5c7d6a3fb66225da3834314f4
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-21 07:41:30 +0300

    Add 0-print_list.c and update lists.h

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * h: pointer to the head of the list_t list to be printed
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t length;
+	const list_t *p;
+
+	length = 0;
+	for (p = h; p != NULL; p = p->next)
+	{
+		if (p->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", p->len, p->str);
+		length++;
+	}
+	return (length);
+}
