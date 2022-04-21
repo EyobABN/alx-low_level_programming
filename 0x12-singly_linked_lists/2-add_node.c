6e0e3306633338c979e0735304938c4ffcfc5787
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-21 19:43:06 +0300

    Add 2-add_node.c and update lists.h

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a node to the beginning of a list_t list
+ * @head: the head of the list_t list
+ * @str: the string to be the str element of the new head
+ *
+ * Return: a pointer to the new list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
