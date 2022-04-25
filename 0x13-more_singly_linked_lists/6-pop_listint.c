bc39ea5167d5cb02c22f69b68d398c11b85b1c26
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-26 00:40:31 +0300

    Add 6-pop_listint.c and update lists.h

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delets the head of a listint_t list
+ * @head: the head of the list
+ *
+ * Return: the head's data, 0 if list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *firstNode;
+
+	if (head == NULL)
+		return (0);
+	firstNode = *head;
+	*head = (*head)->next;
+	n = firstNode->n;
+	free(firstNode);
+	return (n);
+}
