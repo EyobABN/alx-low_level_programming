51b665014146755c0378d238ce14eea72c964343
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-25 16:06:49 +0300

    Add 4-free_listint.c and update lists.h

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: the head of the list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	if (head->next == NULL)
+		free(head);
+	else
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
