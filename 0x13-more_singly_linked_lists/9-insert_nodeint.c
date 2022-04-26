6440848f8e72627617150444e85bfe28c638e217
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-26 03:12:32 +0300

    Rework exception handling

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	for (; p != NULL; p = p->next)
 		count++;
-	if (idx >= count)
+	if (idx > count)
 		return (NULL);
 	p = *head;
 	for (i = 0; i < idx; i++)
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (p == NULL)
+	if (*head == NULL)
 	{
 		p = new;
 		p->next = NULL;
