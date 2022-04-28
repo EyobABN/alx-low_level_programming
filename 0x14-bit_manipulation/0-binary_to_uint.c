b7725b539a90d5e809400c557477d47e40a22212
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-28 16:48:55 +0300

    Rework to utilise bitwise operations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, count, j, acc, base = 2, power = 0;
-	int i;
+	unsigned int i, count = 0, num = 0;
 
 	if (b == NULL)
 		return (0);
-	count = 0;
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
 		count++;
 	}
-	num = 0;
-	for (i = count - 1; i >= 0; i--)
+	for (i = 0; i < count; i++)
 	{
-		acc = 1;
-		for (j = 0; j < power; j++)
-			acc *= base;
-		num += (b[i] - '0') * acc;
-		power++;
+		num = num << 1;
+		if (b[i] == '1')
+			num++;
 	}
+
 	return (num);
 }
