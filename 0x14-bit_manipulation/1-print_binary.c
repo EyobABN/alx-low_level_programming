dfcc2b143b591a59a35314755c634f02aa482789
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-28 19:32:14 +0300

    Add 1-print_binary.c and update main.h

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,19 @@
  */
 void print_binary(unsigned long int n)
 {
-	int size = sizeof(unsigned long int) * 8;
-	unsigned int arr[sizeof(unsigned long int) * 8];
-	int i, start = 0;
+	unsigned long int a = 1;
+	int i, shift, digit, size, start = 0;
 
-	for (i = size - 1; i >= 0; i--)
-	{
-		arr[i] = n & 1;
-		n = n >> 1;
-	}
+	size = sizeof(unsigned long int) * 8;
+
 	for (i = 0; i < size; i++)
 	{
-		if (!start && arr[i] == 0 && i != size - 1)
+		shift = size - 1 - i;
+		digit = ((a << shift) & n) >> shift;
+		if (!start && digit == 0 && i != size - 1)
 			continue;
-		if (!start && arr[i] == 1)
+		if (!start && digit == 1)
 			start = 1;
-		_putchar(arr[i] + '0');
+		_putchar(digit + '0');
 	}
 }
