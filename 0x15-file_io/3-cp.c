61f98d3723ce3f882e5adde385228e02143cee13
EyobABN <eyob.alamerew.belay@gmail.com>
2022-05-03 03:17:19 +0300

    Refactor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,6 +7,49 @@
 #include <unistd.h>
 
 /**
+ * error97 - handles error 97
+ *
+ * Return: void.
+ */
+void error97(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
+ * error98 - handles error 98
+ * @f: the file name
+ *
+ * Return: void.
+ */
+void error98(char *f)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f);
+	exit(98);
+}
+/**
+ * error99 - handles error 99
+ * @f: the file name
+ *
+ * Return: void.
+ */
+void error99(char *f)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
+	exit(99);
+}
+/**
+ * error100 - handles error 100
+ * @fd: the file descriptor
+ *
+ * Return: void.
+ */
+void error100(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+/**
  * main - check the code
  * @ac: argument count
  * @av: argument vector
@@ -20,38 +63,23 @@ int main(int ac, char **av)
 	int fd[2], close_fd[2], i;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
-		exit(97);
-	}
+		error97();
 	fd[0] = open(av[1], O_RDONLY);
 	if (fd[0] == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		error98(av[1]);
+	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
 	if (fd[1] == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	while (1)
-	{
-		read_size1 = read(fd[0], buf, sizeof(buf));
-		write_size2 = write(fd[1], buf, read_size1);
-		if (write_size2 != read_size1)
-			break;
-	}
+		error99(av[2]);
+	read_size1 = read(fd[0], buf, sizeof(buf));
+	if (read_size1 == -1)
+		error98(av[1]);
+	write_size2 = write(fd[1], buf, read_size1);
+	if (write_size2 == -1)
+		error99(av[2]);
 	close_fd[0] = close(fd[0]);
 	close_fd[1] = close(fd[1]);
 	for (i = 0; i < 2; i++)
-	{
 		if (close_fd[i] == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd[i]);
-			exit(100);
-		}
-	}
+			error100(fd[i]);
 	return (0);
 }
