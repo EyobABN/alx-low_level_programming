e5de2418ed8d52fdc42e4c9fe1ba07450d913c56
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-01 20:42:35 +0300

    Handle exception

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,8 +12,10 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *p;
 	unsigned int i, first = 1;
 
+	if (ht == NULL)
+		return;
 	putchar('{');
-	if (ht != NULL && ht->size != 0 && ht->array != NULL)
+	if (ht->size != 0 && ht->array != NULL)
 		for (i = 0; i < ht->size; i++)
 		{
 			p = (ht->array)[i];
