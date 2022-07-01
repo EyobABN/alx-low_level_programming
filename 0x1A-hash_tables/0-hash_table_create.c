75834f2ebd3c9f40e399841dcb693de0399c5853
EyobABN <eyob.alamerew.belay@gmail.com>
2022-07-01 09:47:52 +0300

    Fix compilation error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_node_t *array;
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = &(malloc(sizeof(hash_node_t) * size));
+	array = malloc(sizeof(hash_node_t) * size);
+	table->array = &array;
 	return (table);
 }
